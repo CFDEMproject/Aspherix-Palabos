ec785c23d9bc79fc7e12d67649357c55505c56d9
Philippe Seil <philippe.seil@dcs-computing.com>
2020-02-17 16:58:02 +0100

    fixed non-dying socket lib with bash trap in Allrun.sh

--- Allrun.sh
+++ Allrun.sh
@@ -9,23 +9,31 @@
 
 trap cleanup 1 2 3 6 15
 
+function cleanup ()
+{
+    pkill -P $DEMPID
+    pkill -P $CFDPID
+#    kill -15 $DEMPID
+#    kill -15 $CFDPID
+}
+
 # start DEM separately
 bash DEMrun.sh &
-BASHPID=$!
+DEMPID=$!
 
 #source DEMrun.sh&
 
-function cleanup ()
-{
-    pkill -P $BASHPID
-    kill -15 $BASHPID
-}
-
 # start CFD ./sedimentingSphere N uMax rho_f mu_f v_inf maxT outDir
-mpirun -np 2 ./sedimentingSphere 5 0.1 970 0.373 2.5 1 ./log 2>&1 | tee log_CFD
+bash -c "mpirun -np 2 ./sedimentingSphere 5 0.1 970 0.373 2.5 1 ./log 2>&1 | tee log_CFD" &
 #./sedimentingSphere 10 1 970 0.373 2.5 1 ./log 2>&1 | tee log_CFD
+CFDPID=$!
 
-cleanup
+while [ $(kill -0 $DEMPID 2> /dev/null; echo $?) == "0" ] && [ $(kill -0 $CFDPID 2> /dev/null; echo $?) == "0" ]
+do
+    sleep 1s
+done
+
+trap - 1 2 3 6 15
 
 echo "clean up? (otherwise ctrl-C)"
 read
